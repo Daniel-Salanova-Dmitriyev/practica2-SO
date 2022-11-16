173fa2f224cd3525119b3b73a505148c3b99350e
Dani Salanova <dasadmx@gmail.com>
2022-11-16 14:26:09 +0100

    act nivel1

--- nivel1.c
+++ nivel1.c
@@ -18,9 +18,61 @@
 #define CYAN_T "\x1b[36m"
 #define BLANCO_T "\x1b[97m"
 #define NEGRITA "\x1b[1m"
-#define DEBUGN1 true
+#define DEBUGN1 1
 
 char const PROMPT = '$';
+
+
+
+int internal_cd(char **args){
+    #if DEBUGN1
+        printf("Comando que nos permitirá cambiar de directorio");
+    #endif
+    return 1;
+}
+
+int internal_export(char **args){
+    #if DEBUGN1
+        printf("Comando que define una variable de entorno");
+    #endif
+    return 1;
+}
+
+int internal_source(char **args){
+    #if DEBUGN1
+        printf("Comando que hace que un proceso se ejecute sin crear un hijo");
+    #endif
+    return 1;
+}
+
+int internal_jobs(char **args){
+    #if DEBUGN1
+        printf("Comando que nos muestra los procesos resultantes de nuestro terminal ");
+    #endif
+    return 1;
+}
+
+int internal_fg(char **args){
+    #if DEBUGN1
+        printf("Comando que mueve un proceso en segundo plano al primer plano");
+    #endif
+    return 1;
+}
+
+int internal_bg(char **args){
+    #if DEBUGN1
+        printf("Comando que reanuda un proceso que esta suspendido en segundo plano");
+    #endif
+    return 1;
+}
+
+
+void imprimir_prompt(){
+    char *usuario = getenv("USER");
+    char *direccion = getenv("PWD");
+
+    printf(ROJO_T "%s:" AZUL_T "%s" BLANCO_T "%c ", usuario, direccion, PROMPT);    
+}
 
 
 char *read_line(char *line){
@@ -45,22 +97,48 @@ char *read_line(char *line){
     return linea;
 }
 
-void imprimir_prompt(){
-    char *usuario = getenv("USER");
-    char *direccion = getenv("PWD");
 
-    printf(ROJO_T "%s:" AZUL_T "%s" BLANCO_T "%c ", usuario, direccion, PROMPT); 
-   
+
+int check_internal(char **args){
+    int retorno;
+    retorno = strcmp(args[0],"cd");//internal_cd() 
+    if (retorno == 0){
+        internal_cd(args);
+        return 1;
+    }
+    retorno = strcmp(args[0],"export");//internal_export()
+    if (retorno == 0){
+        internal_export(args);
+        return 1;
+    }
+    retorno = strcmp(args[0],"source");//internal_source() 
+    if (retorno == 0){
+        internal_source(args);
+        return 1;
+    }
+    retorno = strcmp(args[0],"jobs");//internal_jobs(),
+    if (retorno == 0){
+        internal_jobs(args);
+        return 1;
+    }
+    retorno = strcmp(args[0],"fg");//internal_fg()
+    if (retorno == 0){
+        internal_fg(args);
+        return 1;
+    }
+    retorno = strcmp(args[0],"bg");//internal_bg()
+    if (retorno == 0){
+        internal_bg(args);
+        return 1;
+    }
+    retorno = strcmp(args[0],"exit");// exit()
+    if (retorno == 0){
+        exit(0);
+        return 1;
+    }
+    return 0;
 }
 
-
-
-int execute_line(char *line){
-    char **args = malloc(ARGS_SIZE);
-    parse_args(args, line);
-    check_internal(args);
-
-}
 
 int parse_args(char **args, char *line){
     char *sep = "\t\n\r ";
@@ -84,85 +162,16 @@ int parse_args(char **args, char *line){
     return i;
 }
 
-/**
- *  while (args[i] != NULL)
-    {
-        if (*args[i] == '#')
-        {
-            args[i] = NULL;
-        }
-        i++;
-        args[i] = strtok(NULL, sep);
-    }
-    return i;
-*/
 
+int execute_line(char *line){
+    char **args = malloc(ARGS_SIZE);
+    parse_args(args, line);
+   
+    check_internal(args);
+    free(line); //liberamos el espacio alojado por la linea
+    free(args); //LIberamos el espacio alojado por los argumentos
 
-int check_internal(char **args){
-    int retorno;
-    retorno = strcmp(args[0],"cd");//internal_cd() 
-    if (retorno == 0){
-        internal_cd(**args);
-        return 1;
-    }
-    retorno = strcmp(args[0],"export");//internal_export()
-    if (retorno == 0){
-        internal_export(**args);
-        return 1;
-    }
-    retorno = strcmp(args[0],"source");//internal_source() 
-    if (retorno == 0){
-        internal_source(**args);
-        return 1;
-    }
-    retorno = strcmp(args[0],"jobs");//internal_jobs(),
-    if (retorno == 0){
-        internal_jobs(**args);
-        return 1;
-    }
-    retorno = strcmp(args[0],"fg");//internal_fg()
-    if (retorno == 0){
-        internal_fg(**args);
-        return 1;
-    }
-    retorno = strcmp(args[0],"bg");//internal_bg()
-    if (retorno == 0){
-        internal_bg(**args);
-        return 1;
-    }
-    retorno = strcmp(args[0],"exit");// exit()
-    if (retorno == 0){
-        exit(0);
-        return 1;
-    }
-    return 0;
 }
-
-int internal_cd(char **args){
-    printf("Comando que nos permitirá cambiar de directorio");
-}
-
-int internal_export(char **args){
-    printf("Comando que define una variable de entorno");
-}
-
-int internal_source(char **args){
-     printf("Comando que hace que un proceso se ejecute sin crear un hijo");
-}
-
-int internal_jobs(char **args){
-     printf("Comando que nos muestra los procesos resultantes de nuestro terminal ");
-}
-
-int internal_fg(char **args){
-     printf("Comando que mueve un proceso en segundo plano al primer plano");
-}
-
-int internal_bg(char **args){
-     printf("Comando que reanuda un proceso que esta suspendido en segundo plano");
-}
-
-
 
 
 /**
